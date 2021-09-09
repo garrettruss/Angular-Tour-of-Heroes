f5bede13241bb35af5351d88207360daed62944a
garrettruss <garrettmruss@gmail.com>
2021-09-09 09:53:00 -0400

    save

---
+++ src/app/hero.service.ts
@@ -0,0 +1,53 @@
+/* 
+Why services
+Components shouldn't fetch or save data directly and they certainly shouldn't knowingly present fake data. They should focus on presenting data and delegate data access to a service.
+
+In this tutorial, you'll create a HeroService that all application classes can use to get heroes. Instead of creating that service with the 
+new
+keyword, you'll rely on Angular dependency injection to inject it into the HeroesComponent constructor.
+
+Services are a great way to share information among classes that don't know each other. You'll create a MessageService and inject it in two places.
+
+Inject in HeroService, which uses the service to send a message.
+Inject in MessagesComponent, which displays that message, and also displays the ID when the user clicks a hero.
+
+@Injectable() services
+Notice that the new service imports the Angular Injectable symbol and annotates the class with the @Injectable() decorator. This marks the class as one that participates in the dependency injection system. The HeroService class is going to provide an injectable service, and it can also have its own injected dependencies.
+
+The @Injectable() decorator accepts a metadata object for the service, the same way the @Component() decorator did for your component classes.
+
+*/
+
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+
+
+/* You must make the HeroService available to the dependency injection system before Angular can inject it into the HeroesComponent by registering a provider. A provider is something that can create or deliver a service; in this case, it instantiates the HeroService class to provide the service.
+
+To make sure that the HeroService can provide this service, register it with the injector, which is the object that is responsible for choosing and injecting the provider where the application requires it.
+
+By default, the Angular CLI command ng generate service registers a provider with the root injector for your service by including provider metadata, that is providedIn: 'root' in the @Injectable() decorator.
+
+When you provide the service at the root level, Angular creates a single, shared instance of HeroService and injects into any class that asks for it. Registering the provider in the @Injectable metadata also allows Angular to optimize an application by removing the service if it turns out not to be used after all.
+*/
+
+@Injectable({
+  providedIn: 'root'
+})
+
+/* The HeroService could get hero data from anywhere—a web service, local storage, or a mock data source. Removing data access from components means you can change your mind about the implementation anytime, without touching any components. They don't know how the service works.
+ */
+
+export class HeroService {
+
+  constructor() { }
+
+  /* Add a getHeroes method to return the mock heroes. of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes. */
+  getHeroes(): Observable<Hero[]> {
+  const heroes = of(HEROES);
+  return heroes;
+}
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+//import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +10,36 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  //define a component property called heroes to expose the HEROES array for binding.
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() { }
+  /* Add a private heroService parameter of type HeroService to the constructor. The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site. When Angular creates a HeroesComponent, the Dependency Injection system sets the heroService parameter to the singleton instance of HeroService. */
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit(): void {
+  /* Create a method to retrieve the heroes from the service.
+
+  The HeroService.getHeroes() method has a synchronous signature, which implies that the HeroService can fetch heroes synchronously. The HeroesComponent consumes the getHeroes() result as if heroes could be fetched synchronously.
+
+  This will not work in a real application. You're getting away with it now because the service currently returns mock heroes. But soon the application will fetch heroes from a remote server, which is an inherently asynchronous operation.
+
+  The HeroService must wait for the server to respond, getHeroes() cannot return immediately with hero data, and the browser will not block while the service waits.
+
+  HeroService.getHeroes() must have an asynchronous signature of some kind.
+
+  The new version waits for the Observable to emit the array of heroes—which could happen now or several minutes from now. The subscribe() method passes the emitted array to the callback, which sets the component's heroes property.
+
+  This asynchronous approach will work when the HeroService requests heroes from the server.
+  
+  */
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  /* While you could call getHeroes() in the constructor, that's not the best practice.Reserve the constructor for minimal initialization such as wiring constructor parameters to properties. The constructor shouldn't do anything. It certainly shouldn't call a function that makes HTTP requests to a remote server as a real data service would. Instead, call getHeroes() inside the ngOnInit lifecycle hook and let Angular call ngOnInit() at an appropriate time after constructing a HeroesComponent instance.*/
+
+  ngOnInit() {
+    this.getHeroes();
   }
 
     // onSelect() method assigns the clicked hero from the template to the component's selectedHero.
